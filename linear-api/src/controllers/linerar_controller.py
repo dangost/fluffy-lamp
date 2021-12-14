d63b467d3fb14a1294dbcbf4a57dbc24ce3ed864
Danila Kislitsyn <dangost16@gmail.com>
2021-12-14 13:53:00 +0300

    eqr

--- linear-api/src/controllers/linerar_controller.py
+++ linear-api/src/controllers/linerar_controller.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify
 
+from src.core.linear_solving import solve_linear
+
 linear_api = Blueprint("linear_controller_api", __name__)
 
 
 @linear_api.route("/api/linear", methods=["GET"])
-def solve_linear():
+def solve_linear_controller():
     response = solve_linear()
     return jsonify(response)
